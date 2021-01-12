13381b6e0d6fc8e12e064995b6ce17dd83f6b696
nfcurti <nicolascurti75@gmail.com>
2021-01-12 21:35:29 +0100

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_01_06_210127) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
