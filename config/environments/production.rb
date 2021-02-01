9ccbfc78ff65d4a387146c90c98a9c70314d99ea
nfcurti <nicolascurti75@gmail.com>
2021-02-01 16:31:41 +0100

    webhook error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address              => 'smtp.elasticemail.com',
-  :port                 => 2525,
+  :port                 => 587,
       :user_name => ENV["SENDGRID_USERNAME"],
       :password => ENV["SENDGRID_PASSWORD"],
   :authentication       => :plain,
