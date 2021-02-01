7fb3bf281f220e5b6cf36e8825031474c323a843
nfcurti <nicolascurti75@gmail.com>
2021-02-01 16:12:03 +0100

    webhook error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address              => 'smtp.mailgun.org',
+  :address              => 'smtp.sendgrid.org',
   :port                 => 587,
       :user_name => ENV["SENDGRID_USERNAME"],
       :password => ENV["SENDGRID_PASSWORD"],
