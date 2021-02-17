01a35442c59ea48279a7d416bca01f2bdb74f824
nfcurti <nicolascurti75@gmail.com>
2021-02-17 15:46:13 +0100

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-	Stripe.api_key = ENV["STRIPE_APIKEY"]
+	#Stripe.api_key = ENV["STRIPE_APIKEY"]
+Stripe.api_key = "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"
 
   private
   def logged_in_user
